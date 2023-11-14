33cbb351ced2419e2c477efa823bd47e41ff9190
VitaliyV22 <vitanskyyv@gmail.com>
2023-11-14 15:16:15 -0600

    Added a way to add a new item in the list

--- app.js
+++ app.js
@@ -1,6 +1,8 @@
 const list = document.querySelector(".list")
 const button = document.querySelectorAll("li button")
 const chore = document.querySelectorAll(".chore")
+const form = document.querySelector(".newTask")
+const input =document.querySelector("#task")
 
 for(let btn of button){
     btn.addEventListener("click", function(e){
@@ -8,5 +10,17 @@ for(let btn of button){
         
     })
 }
+
+form.addEventListener("submit", function(e){
+    e.preventDefault()
+    console.log(input.value)
+    const newChore = document.createElement("li")
+    newChore.innerText = input.value
+    input.value = ""
+    list.appendChild(newChore)
+
+})
+
+
 
 
