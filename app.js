3d302c9e9b02948f62522b91ddbddba53e8ebfcb
VitaliyV22 <vitanskyyv@gmail.com>
2023-11-14 16:44:12 -0600

    working on localStorage

--- app.js
+++ app.js
@@ -1,30 +1,26 @@
-const list = document.querySelector(".list")
-const form = document.querySelector(".newTask")
-const input =document.querySelector("#task")
+const list = document.querySelector(".list");
+const form = document.querySelector(".newTask");
+const input = document.querySelector("#task");
 
+form.addEventListener("submit", function (e) {
+  e.preventDefault();
+  const newChore = document.createElement("li");
+  const newBtn = document.createElement("button");
+  newBtn.innerText = "Done";
+  newChore.innerText = input.value;
+  newChore.appendChild(newBtn);
+  input.value = "";
+  list.appendChild(newChore);
 
+let savedTasks = JSON.parse(localStorage.getItem("tasks")) || []
 
-form.addEventListener("submit", function(e){
-    e.preventDefault()
-    const newChore = document.createElement("li")
-    const newBtn= document.createElement("button")
-    newBtn.innerText = "Done"
-    newChore.innerText =  input.value
-    newChore.appendChild(newBtn)
-    input.value = ""
-    list.appendChild(newChore)
-    
+savedTasks.push({newChore})
+localStorage.setItem("tasks", JSON.stringify(savedTasks))
 
-})
-  
-list.addEventListener("click",function(e){
-    if (e.target.tagName  === "BUTTON") {
-        e.target.parentElement.remove()
-    }
-})
+});
 
-
-
-
-
-
+list.addEventListener("click", function (e) {
+  if (e.target.tagName === "BUTTON") {
+    e.target.parentElement.remove();
+  }
+});
