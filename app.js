0c2da06d01229252fdd19a0cab3fa9c01e66c281
VitaliyV22 <vitanskyyv@gmail.com>
2023-11-15 14:13:01 -0600

    added function to refresh localstorage and save new task

--- app.js
+++ app.js
@@ -16,4 +16,36 @@ function setItems(items) {
   localStorage.setItem("todo", itemsJson);
 }
 
-
+function addItem() {
+    items.unshift({
+      description: "",
+      completed: false
+    });
+  
+    setItems(items);
+    refreshList();
+  }
+  
+  
+  
+  function refreshList() {
+    ItemsContainer.innerHTML = "";
+  
+    for (const item of items) {
+      const itemElement = ItemTemplate.content.cloneNode(true);
+      const descriptionInput = itemElement.querySelector(".item-description");
+      const completedInput = itemElement.querySelector(".item-completed");
+  
+      descriptionInput.value = item.description;
+      completedInput.checked = item.completed;
+  
+      ItemsContainer.append(itemElement);
+    }
+  }
+  
+  
+  
+  addButton.addEventListener("click", () => {
+      addItem()
+  })
+  
